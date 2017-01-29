1712e8603a4cff4d4cc9dd8215d2cfd5a50b5e92
Andy Guttridge <andy@andygutidgesPro.home>
2017-01-29 15:39:32 +0000

    Added RTF file containing instructions to the project files; Added a UITextView and code to display the content of the RTF instructions file in the info view; Changed constants for amount of time for pre-moon date notification and allowed time to release journal text after a moon date to real value (3 days for advance notifications and 4 days for time allowed to release journal entries).

--- Moon Dates/WWFinfoViewController.m
+++ Moon Dates/WWFinfoViewController.m
@@ -10,13 +10,26 @@
 
 @interface WWFinfoViewController ()
 
+@property IBOutlet UITextView *infoView; //The UITextView used to display instructions for the app
+@property (copy) NSAttributedString *infoText; //The text for the instructions for the app
+
 @end
 
 @implementation WWFinfoViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"mj_app_instructions" ofType:@"rtf"]; //Find the path to the RTF file containing the instructions for the app
+    NSData *instructionsRTF = [NSData dataWithContentsOfFile:path]; //Load the RTF file into an NSData object
+    NSDictionary *attributesForInitialisingNSAttributedString = @{NSDocumentTypeDocumentAttribute: NSRTFTextDocumentType}; //Create a NSDictionary containing an atributes dictionary for use when creating an NSAttributedString. The attribute within this NSDictionary tells NSAttributedString that we are creating an attributed string from a RTF document.
+    
+    self.infoText = [[NSAttributedString alloc] initWithData: instructionsRTF options:attributesForInitialisingNSAttributedString documentAttributes:NULL error:NULL]; //Create an NSAttributedString from the RTF.
+    
+    self.infoView.attributedText = _infoText; //Set our UITextViews attributedText property to our NSAttributedString containing the text from our RTF.
+    
 }
 
 - (void)didReceiveMemoryWarning {
