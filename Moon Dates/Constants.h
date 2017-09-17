5bb1a9eb4adabb201eb48d76f5c0d730f576501b
Andy Guttridge <andy@ALs-MacBook-Pro.home>
2017-09-17 11:57:47 +0100

    Set constants to normal operating values, set not to generate test data.

--- Moon Dates/Constants.h
+++ Moon Dates/Constants.h
@@ -15,20 +15,20 @@ typedef enum {kNoMoonEvent, kFullMoon, kNewMoon} moonType;
 
 //Use a value of -259,200 for NotificationOffset in the UserData.plist file for a default of three days in advance of the moon date for the calendar view to highlight in blue.
 
-//#define kAllowedLetItGoInterval -345600 //The amount of time after a moon event within which the relevant journal entry can be released in seconds. Use this for 4 days.
+#define kAllowedLetItGoInterval -345600 //The amount of time after a moon event within which the relevant journal entry can be released in seconds. Use this for 4 days.
 
 //#define kAllowedLetItGoInterval -28800 //The amount of time after a moon event within which the relevant journal entry can be released in seconds. Use this for 8 hours.
 //#define kAllowedLetItGoInterval -43200 //The amount of time after a moon event within which the relevant journal entry can be released in seconds. Use this for 12 hours.
 //#define kAllowedLetItGoInterval -172800 //The amount of time after a moon event within which the relevant journal entry can be released in seconds. Use this for 2 days.
-#define kAllowedLetItGoInterval -900 //The amount of time after a moon event within which the relevant journal entry can be released in seconds. Use this for 15 minutes.
+//#define kAllowedLetItGoInterval -900 //The amount of time after a moon event within which the relevant journal entry can be released in seconds. Use this for 15 minutes.
 //#define kAllowedLetItGoInterval -60 //The amount of time after a moon event within which the relevant journal entry can be released in seconds. Use this for one minute.
 //#define kAllowedLetItGoInterval -30 //The amount of time after a moon event within which the relevant journal entry can be released in seconds. Use this for 30 seconds.
 
 
 
-//#define kpreMoonDateLetItGoInterval 86400 //The amount of time before the moon date within which the relevant journal entry can be released in seconds. Use this for 24 hours.
+#define kpreMoonDateLetItGoInterval 86400 //The amount of time before the moon date within which the relevant journal entry can be released in seconds. Use this for 24 hours.
 //#define kpreMoonDateLetItGoInterval 60 //The amount of time before the moon date within which the relevant journal entry can be released in seconds. Use this for 60 seconds for testing.
-#define kpreMoonDateLetItGoInterval 900 //The amount of time before the moon date within which the relevant journal entry can be released in seconds. Use this for 15 minutes for testing.
+//#define kpreMoonDateLetItGoInterval 900 //The amount of time before the moon date within which the relevant journal entry can be released in seconds. Use this for 15 minutes for testing.
 
 #define kTestDataDateString "2016-08-20 13:00:00" //A string to use with an NSDateFormatter to generate an NSDate from a string, when generating test data.
 
